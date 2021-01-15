203c7d0978c399c642d77ecc7aa7e3e38a4d56bd
astsu <you@example.com>
2021-01-15 14:59:47 +0200

    Splitted hitbtc auth intereface the sign method url argument to url_path and url_query.

--- exapi/requesters/hitbtc/auth/interface.py
+++ exapi/requesters/hitbtc/auth/interface.py
@@ -1,22 +1,26 @@
 """Has hitbtc authorizator interface."""
 
-from typing import Protocol
+from typing import Optional, Protocol
 
-from exapi.requesters.typedefs import Headers
-from yarl import URL
+from .typedefs import HitbtcAuthHeaders
 
 
 class IHitbtcAuth(Protocol):
-    """Has methods for authorize a request to exchange."""
+    """Has methods for authorization a request to exchange."""
 
-    def sign(self, method: str, url: URL, body: str) -> Headers:
+    def sign(self, method: str,
+             url_path: str,
+             url_query: str,
+             body: Optional[str] = None
+             ) -> HitbtcAuthHeaders:
         """Returns headers with the Authorization header.
 
         Args:
             method (str): http method.
-            url (URL): http url.
+            url_path (str): url path.
+            url_query (str): url query.
             body (str): http body.
 
         Returns:
-            Headers
+            HitbtcAuthHeaders
         """
