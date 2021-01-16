5146a9b6da9d0a2740786543c154803576d566ca
astsu <you@example.com>
2021-01-16 10:38:01 +0200

    Added hitbtc public requester class.

---
+++ exapi/requesters/hitbtc/requester.py
@@ -0,0 +1,18 @@
+"""Has abstract requester classes for hitbtc requesters."""
+
+from typing import Optional
+
+import aiohttp
+from exapi.requesters.requester import BaseRequester
+
+from .interfaces import IHitbtcPublicRequester
+
+
+class HitbtcPublicRequester(BaseRequester, IHitbtcPublicRequester):
+    """Hitbtc public requester which inherites base requester.
+
+    Handles session in init.
+    """
+
+    def __init__(self, session: Optional[aiohttp.ClientSession] = None) -> None:
+        self._session = session if session is not None else aiohttp.ClientSession()
