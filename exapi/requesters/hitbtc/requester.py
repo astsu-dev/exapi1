75f84e4ee73edd5ac63862aafdede71b8cc574a7
astsu <you@example.com>
2021-01-19 16:27:51 +0200

    Added hitbtc base requester. Now hitbtc public requester has name HitbtcBasePublicRequester and inherites hitbtc base requester.

--- exapi/requesters/hitbtc/requester.py
+++ exapi/requesters/hitbtc/requester.py
@@ -1,13 +1,22 @@
 """Has abstract requester classes for hitbtc requesters."""
 
-from typing import Optional
+from typing import Final, Optional
 
 import aiohttp
 from exapi.requesters.requester import BaseRequester
 
 
-class HitbtcPublicRequester(BaseRequester):
-    """Hitbtc public requester which inherites base requester.
+class HitbtcBaseRequester(BaseRequester):
+    """Hitbtc base requester.
+
+    Has ROOT_URL field which contains hitbtc api url.
+    """
+
+    ROOT_URL: Final[str] = "https://api.hitbtc.com/api/2"
+
+
+class HitbtcBasePublicRequester(HitbtcBaseRequester):
+    """Hitbtc public requester which inherites hitbtc base requester.
 
     Handles session in init.
     """
