0abfa8f4e7d6b8acef9e98af6b19324efc680d25
astsu <you@example.com>
2021-01-24 14:05:20 +0200

    Added hitbtc market data requester. Added tests for him.

--- exapi/requesters/hitbtc/market_data/__init__.py
+++ exapi/requesters/hitbtc/market_data/__init__.py
@@ -1,1 +1,4 @@
 from .interface import IHitbtcMarketDataRequester
+from .request_creator import (HitbtcMarketDataRequestCreator,
+                              IHitbtcMarketDataRequestCreator)
+from .requester import HitbtcMarketDataRequester
