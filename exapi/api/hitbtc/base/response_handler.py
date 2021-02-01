c6d88762d219092541d1e0aa1dc60da2ea50dee8
astsu <you@example.com>
2021-02-01 21:47:04 +0200

    Added models_mapper argument to hitbtc base response handler __init__ method. Hitbtc base response handler _check_json_response method used hitbtc base models mapper to map error.

--- exapi/api/hitbtc/base/response_handler.py
+++ exapi/api/hitbtc/base/response_handler.py
@@ -1,17 +1,24 @@
 """Has base hitbtc response handler."""
 
-from typing import Any, Optional
+from typing import Any, Optional, Union
 
 from exapi.api.base import BaseResponseHandler
 from exapi.api.hitbtc.exceptions import HitbtcError
-from exapi.api.hitbtc.models import HitbtcErrorModel
+from exapi.api.hitbtc.models import (HitbtcRawDetailedErrorModel,
+                                     HitbtcRawErrorModel)
 from exapi.requesters.typedefs import RequesterResponse
+
+from .models_mapper import HitbtcBaseModelsMapper, IHitbtcBaseModelsMapper
 
 
 class HitbtcBaseResponseHandler(BaseResponseHandler):
     """Has methods for handle aiohttp response."""
 
-    def __init__(self, json_content_type: Optional[str] = "application/json") -> None:
+    def __init__(self, models_mapper: Optional[IHitbtcBaseModelsMapper] = None,
+                 json_content_type: Optional[str] = "application/json"
+                 ) -> None:
+        self._models_mapper = (
+            models_mapper if models_mapper is not None else HitbtcBaseModelsMapper())
         self._json_content_type = json_content_type
 
     def _check_response(self, response: RequesterResponse) -> Any:
@@ -32,6 +39,9 @@ class HitbtcBaseResponseHandler(BaseResponseHandler):
         Args:
             response (Any): json response.
 
+        Raises:
+            HitbtcError: will be raised if response is error.
+
         Returns:
             Any
         """
@@ -39,9 +49,7 @@ class HitbtcBaseResponseHandler(BaseResponseHandler):
         if not isinstance(response, dict) and "error" not in response:
             return None
 
-        error = response["error"]
-        code = int(error["code"])
-        msg = error["message"]
-        description = error.get("description")
-        model = HitbtcErrorModel(code, msg, description)
-        raise HitbtcError(code, model)
+        raw_error: Union[HitbtcRawDetailedErrorModel,
+                         HitbtcRawErrorModel] = response["error"]
+        error = self._models_mapper.map_to_error(raw_error)
+        raise HitbtcError(error.code, error)
