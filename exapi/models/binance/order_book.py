14ac5fce326a2d0d721f8542580b8e9e77aebe87
astsu <you@example.com>
2021-02-14 22:05:34 +0200

    Add documentation to binance order model.

--- exapi/models/binance/order_book.py
+++ exapi/models/binance/order_book.py
@@ -2,9 +2,9 @@
 
 from dataclasses import dataclass
 from decimal import Decimal
-from typing import List, TypedDict
+from typing import List, Tuple, TypedDict
 
-BinanceOrderBookOrderJson = List[str]
+BinanceOrderBookOrderJson = Tuple[str, str]
 BinanceOrderBookOrdersJson = List[BinanceOrderBookOrderJson]
 
 
@@ -32,6 +32,14 @@ BinanceOrderBookOrders = List[BinanceOrderBookOrderModel]
 
 @dataclass(frozen=True)
 class BinanceOrderBookModel:
+    """Order book model.
+
+    Args:
+        last_update_id (int)
+        bids (BinanceOrderBookOrders)
+        asks (BinanceOrderBookOrders)
+    """
+
     last_update_id: int
     bids: BinanceOrderBookOrders
     asks: BinanceOrderBookOrders
