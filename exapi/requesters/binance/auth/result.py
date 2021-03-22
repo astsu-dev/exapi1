636e7b7893bd8b3337eeb8466448cc30e29d0220
astsu <you@example.com>
2021-03-22 14:36:39 +0200

    Added binance auth result dataclass.

---
+++ exapi/requesters/binance/auth/result.py
@@ -0,0 +1,20 @@
+"""Has binance auth result dataclass."""
+
+from dataclasses import dataclass
+
+from exapi.requesters.typedefs import Params
+
+from .typedefs import BinanceAuthHeaders
+
+
+@dataclass(frozen=True)
+class BinanceAuthResult:
+    """Binance auth result.
+
+    Args:
+        headers (BinanceAuthHeaders): headers with api key.
+        params (Params): params with signature.
+    """
+
+    headers: BinanceAuthHeaders
+    params: Params
