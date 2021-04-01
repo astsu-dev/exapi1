8d4d4ef0a65b0d39ed2efc6ea0d2ff454d7049b9
astsu <you@example.com>
2021-04-01 09:34:43 +0300

    Added binance order info model.

--- exapi/models/binance/order.py
+++ exapi/models/binance/order.py
@@ -57,6 +57,24 @@ class BinanceFullOrderJson(BinanceResultOrderJson):
     fills: BinanceFilledOrdersJson
 
 
+class BinanceOrderInfoJson(TypedDict):
+    symbol: str
+    orderId: int
+    orderListId: int
+    clientOrderId: str
+    price: str
+    origQty: str
+    executedQty: str
+    cummulativeQuoteQty: str
+    status: OrderStatus
+    timeInForce: TimeInForce
+    type: OrderType
+    side: OrderSide
+    isWorking: bool
+    updateTime: int
+    fills: BinanceFilledOrdersJson
+
+
 @dataclass(frozen=True)
 class BinanceOrderModel:
     """Binance order model.
@@ -94,6 +112,47 @@ class BinanceOrderModel:
     fills: Optional[BinanceFilledOrders] = None
 
 
+@dataclass(frozen=True)
+class BinanceOrderInfoModel:
+    """Binance order info model.
+
+    Used in query order, get all open orders resopnses.
+
+    Args:
+        symbol (str)
+        order_id (int)
+        order_list_id (int)
+        client_order_id (str)
+        is_working: bool
+        update_time: int
+        price (Optional[Decimal], optional): Defaults to None.
+        orig_qty (Optional[Decimal], optional): Defaults to None.
+        executed_qty (Optional[Decimal], optional): Defaults to None.
+        cummulative_quote_qty (Optional[Decimal], optional): Defaults to None.
+        status (Optional[OrderStatus], optional): Defaults to None.
+        time_in_force (Optional[TimeInForce], optional): Defaults to None.
+        type (Optional[OrderType], optional): Defaults to None.
+        side (Optional[OrderSide], optional): Defaults to None.
+        fills (Optional[BinanceFilledOrders], optional): Defaults to None.
+    """
+
+    symbol: str
+    order_id: int
+    order_list_id: int
+    client_order_id: str
+    is_working: bool
+    update_time: int
+    price: Optional[Decimal] = None
+    orig_qty: Optional[Decimal] = None
+    executed_qty: Optional[Decimal] = None
+    cummulative_quote_qty: Optional[Decimal] = None
+    status: Optional[OrderStatus] = None
+    time_in_force: Optional[TimeInForce] = None
+    type: Optional[OrderType] = None
+    side: Optional[OrderSide] = None
+    fills: Optional[BinanceFilledOrders] = None
+
+
 BinanceOrderJson = Union[
     BinanceAckOrderJson,
     BinanceResultOrderJson,
@@ -101,3 +160,5 @@ BinanceOrderJson = Union[
 ]
 BinanceOrdersJson = List[BinanceOrderJson]
 BinanceOrders = List[BinanceOrderModel]
+BinanceOrderInfosJson = List[BinanceOrderInfoJson]
+BinanceOrderInfos = List[BinanceOrderInfoModel]
