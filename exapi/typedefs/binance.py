ff76e6223aeb18e568588658d880b65d50adf15d
astsu <you@example.com>
2021-04-14 19:36:01 +0300

    PEP 8.

--- exapi/typedefs/binance.py
+++ exapi/typedefs/binance.py
@@ -2,63 +2,19 @@
 
 from typing import List, Literal
 
-OrderType = Literal[
-    "LIMIT",
-    "MARKET",
-    "STOP_LOSS",
-    "STOP_LOSS_LIMIT",
-    "TAKE_PROFIT",
-    "TAKE_PROFIT_LIMIT",
-    "LIMIT_MAKER"
-]
+OrderType = Literal["LIMIT", "MARKET", "STOP_LOSS", "STOP_LOSS_LIMIT",
+                    "TAKE_PROFIT", "TAKE_PROFIT_LIMIT", "LIMIT_MAKER"]
 OrderTypes = List[OrderType]
-OrderStatus = Literal[
-    "NEW",
-    "PARTIALLY_FILLED",
-    "FILLED",
-    "CANCELED",
-    "PENDING_CANCEL",
-    "REJECTED",
-    "EXPIRED"
-]
+OrderStatus = Literal["NEW", "PARTIALLY_FILLED", "FILLED",
+                      "CANCELED", "PENDING_CANCEL", "REJECTED", "EXPIRED"]
 OrderSide = Literal["BUY", "SELL"]
 OrderResponseType = Literal["ACK", "RESULT", "FULL"]
-TimeInForce = Literal[
-    "GTC",
-    "IOC",
-    "FOK"
-]
-CandleInterval = Literal[
-    "1m",
-    "3m",
-    "5m",
-    "15m",
-    "30m",
-    "1h",
-    "2h",
-    "4h",
-    "6h",
-    "8h",
-    "12h",
-    "1d",
-    "3d",
-    "1w",
-    "1M"
-]
+TimeInForce = Literal["GTC", "IOC", "FOK"]
+CandleInterval = Literal["1m", "3m", "5m", "15m", "30m", "1h", "2h",
+                         "4h", "6h", "8h", "12h", "1d", "3d", "1w", "1M"]
 SymbolPermission = Literal["SPOT", "MARGIN"]
 SymbolPermissions = List[SymbolPermission]
-SymbolStatus = Literal[
-    "TRADING",
-    "PRE_TRADING",
-    "POST_TRADING",
-    "END_OF_DAY",
-    "HALT",
-    "AUCTION_MATCH",
-    "BREAK"
-]
-RateLimitInterval = Literal[
-    "SECOND",
-    "MINUTE",
-    "DAY"
-]
+SymbolStatus = Literal["TRADING", "PRE_TRADING", "POST_TRADING",
+                       "END_OF_DAY", "HALT", "AUCTION_MATCH", "BREAK"]
+RateLimitInterval = Literal["SECOND", "MINUTE", "DAY"]
 AccountType = Literal["SPOT"]
--- exapi/typedefs/hitbtc.py
+++ exapi/typedefs/hitbtc.py
@@ -10,10 +10,10 @@ SortDirection = Literal["ASC", "DESC"]
 SortBy = Literal["id", "timestamp"]
 Datetime = str
 IntervalValue = Union[Datetime, int]
-CandlesPeriod = Literal["M1", "M3", "M5",
-                        "M15", "M30", "H1", "H4", "D1", "D7", "1M"]
+CandlesPeriod = Literal["M1", "M3", "M5", "M15", "M30",
+                        "H1", "H4", "D1", "D7", "1M"]
 OrderSide = Literal["buy", "sell"]
 OrderType = Literal["limit", "market", "stopLimit", "stopMarket"]
 TimeInForce = Literal["GTC", "IOC", "FOK", "Day", "GTD"]
-OrderStatus = Literal["new", "suspended",
-                      "partiallyFilled", "filled", "canceled", "expired"]
+OrderStatus = Literal["new", "suspended", "partiallyFilled",
+                      "filled", "canceled", "expired"]
